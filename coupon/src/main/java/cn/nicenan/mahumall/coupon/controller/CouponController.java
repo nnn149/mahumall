38ace7fbad5ff316359f902e1dcb1ddd5c4c7f2f
Nannan <1041836312@qq.com>
2021-08-22 01:44:07 +0800

    Nacos 测试配置中心

--- coupon/src/main/java/cn/nicenan/mahumall/coupon/controller/CouponController.java
+++ coupon/src/main/java/cn/nicenan/mahumall/coupon/controller/CouponController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,13 @@ import cn.nicenan.mahumall.coupon.service.CouponService;
 import cn.nicenan.mahumall.common.utils.PageUtils;
 import cn.nicenan.mahumall.common.utils.R;
 
+/**
+ * 使用Nacos作为配置中心统一管理配置
+ * 1. 引入依赖  https://github.com/alibaba/spring-cloud-alibaba/blob/master/spring-cloud-alibaba-examples/nacos-example/nacos-config-example/readme-zh.md
+ * 2. 创建bootstrap.properties文件，这是springboot的文件，配置应用名字和Nacos Server地址
+ * 3. 进入Nacos Server的配置中心，新建配置。Data ID一定是 xxx.properties。xxx是应用名字。默认规则 应用名.properties
+ * 4. 添加任意配置
+ * 5. 动态获取配置，使用两个注解@RefreshScope和  @Value("${配置项名字}")。优先使用配置中心内的值
 
 /**
  * 优惠券信息
@@ -25,11 +34,23 @@ import cn.nicenan.mahumall.common.utils.R;
  * @email 1041836312@qq.com
  * @date 2021-08-21 21:50:24
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${test.user.name}")
+    private String name;
+    @Value("${test.user.age}")
+    private Integer age;
+
+
+    @RequestMapping("/test")
+    public R test() {
+        return R.ok().put("name", name).put("age", age);
+    }
 
     @RequestMapping("/member/list")
     public R memberCoupons() {
