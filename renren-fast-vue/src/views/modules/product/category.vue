5c828ab85726655a7b36c0ff042d8c65fb5c1081
Nannan <1041836312@qq.com>
2021-08-26 14:46:31 +0800

    菜单管理增删改查

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -15,19 +15,19 @@
             type="text"
             size="mini"
             @click="() => append(data)"
-          >Append</el-button>
-          <el-button type="text" size="mini" @click="edit(data)">edit</el-button>
+          >添加</el-button>
+          <el-button type="text" size="mini" @click="edit(data)">修改</el-button>
           <el-button
-            v-if="node.childNodes.length==0"
+            v-if="node.childNodes.length===0"
             type="text"
             size="mini"
             @click="() => remove(node, data)"
-          >Delete</el-button>
+          >删除</el-button>
         </span>
       </span>
     </el-tree>
     <el-dialog
-      :title="title"
+      :title="dialogTitle"
       :visible.sync="dialogVisible"
       width="30%"
       :close-on-click-modal="false"
@@ -56,6 +56,8 @@
 export default {
   data () {
     return {
+      dialogTitle: '添加',
+      dialogType: '',
       category: {
         name: '',
         parentCid: 0,
@@ -84,17 +86,60 @@ export default {
       this.$http({
         url: this.$http.adornUrl('/product/category/list/tree'),
         method: 'get'
-      }).then(({ data }) => {
-        console.log('成功获取到菜单数据...', data.data)
+      }).then(({data}) => {
         this.menus = data.data
       })
     },
     append (data) {
+      this.dialogType = 'add'
       this.dialogVisible = true
+      this.dialogTitle = '添加'
+      this.category.catId = null
+      this.category.parentCid = data.catId
+      this.category.catLevel = data.catLevel * 1 + 1
+      this.category.name = ''
+      this.category.showStatus = 1
+      this.category.sort = 0
+      this.category.productUnit = ''
+      this.category.icon = ''
     },
     // 添加三级分类
     addCategory () {
-
+      this.$http({
+        url: this.$http.adornUrl('/product/category/save'),
+        method: 'post',
+        data: this.$http.adornData(this.category, false)
+      }).then(({data}) => {
+        this.$message({
+          message: '菜单添加成功',
+          type: 'success'
+        })
+        // 刷新出新的菜单
+        this.getMenus()
+        this.dialogVisible = false
+        // 设置需要默认展开的菜单
+        this.expandedKey = [this.category.parentCid]
+      })
+    },
+    editCategory () {
+      let {catId, icon, productUnit, name} = this.category
+      this.$http({
+        url: this.$http.adornUrl('/product/category/update'),
+        method: 'post',
+        data: this.$http.adornData({catId, icon, productUnit, name}, false)
+      }).then(({status}) => {
+        if (status === 200) {
+          this.$message({
+            message: '菜单修改成功',
+            type: 'success'
+          })
+        // 刷新出新的菜单
+          this.getMenus()
+          this.dialogVisible = false
+        // 设置需要默认展开的菜单
+          this.expandedKey = [this.category.parentCid]
+        }
+      })
     },
     remove (node, data) {
       let ids = [data.catId]
@@ -121,6 +166,29 @@ export default {
         })
         .catch(() => {
         })
+    },
+    submitData () {
+      if (this.dialogType === 'add') {
+        this.addCategory()
+      }
+      if (this.dialogType === 'edit') {
+        this.editCategory()
+      }
+    },
+    edit (data) {
+      this.dialogType = 'edit'
+      this.dialogVisible = true
+      this.category.catId = data.catId
+      this.dialogTitle = '编辑'
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: 'get'
+      }).then(({data}) => {
+        this.category.name = data.data.name
+        this.category.icon = data.data.icon
+        this.category.productUnit = data.data.productUnit
+        this.category.parentCid = data.data.parentCid
+      })
     }
 
   },
