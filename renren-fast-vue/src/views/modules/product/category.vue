9467aa3040d4c1720dcb86f73e0df9a3914f50ef
Nannan <1041836312@qq.com>
2021-08-26 09:28:16 +0800

    菜单管理2

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,15 +1,77 @@
 <template>
-  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <div>
+    <el-tree :data="menus"
+             :props="defaultProps"
+             :expand-on-click-node="false"
+             show-checkbox
+             node-key="catId"
+             :default-expanded-keys="expandedKey">
+
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <=2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >Append</el-button>
+          <el-button type="text" size="mini" @click="edit(data)">edit</el-button>
+          <el-button
+            v-if="node.childNodes.length==0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >Delete</el-button>
+        </span>
+      </span>
+    </el-tree>
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :close-on-click-modal="false"
+    >
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input v-model="category.productUnit" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+
 
 </template>
 <script>
 export default {
   data () {
     return {
-      data: [],
+      category: {
+        name: '',
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        productUnit: '',
+        icon: '',
+        catId: null
+      },
+      dialogVisible: false,
+      menus: [],
+      expandedKey: [],
       defaultProps: {
         children: 'children',
-        label: 'label'
+        label: 'name'
       }
     }
   },
@@ -17,21 +79,53 @@ export default {
     this.getMenus()
   },
   methods: {
-    handleNodeClick (data) {
-      console.log(data)
-    },
+
     getMenus () {
       this.$http({
         url: this.$http.adornUrl('/product/category/list/tree'),
         method: 'get'
-      }).then(({data}) => {
-        if (data && data.code === 0) {
-          this.data = data.data
-        } else {
-          this.dataList = []
-        }
+      }).then(({ data }) => {
+        console.log('成功获取到菜单数据...', data.data)
+        this.menus = data.data
       })
+    },
+    append (data) {
+      this.dialogVisible = true
+    },
+    // 添加三级分类
+    addCategory () {
+
+    },
+    remove (node, data) {
+      let ids = [data.catId]
+      this.$confirm(`是否删除【${data.name}】菜单?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl('/product/category/delete'),
+            method: 'post',
+            data: this.$http.adornData(ids, false)
+          }).then(({data}) => {
+            this.$message({
+              message: '菜单删除成功',
+              type: 'success'
+            })
+            // 刷新出新的菜单
+            this.getMenus()
+            // 设置需要默认展开的菜单
+            this.expandedKey = [node.parent.data.catId]
+          })
+        })
+        .catch(() => {
+        })
     }
+
+  },
+  created () {
+    this.getMenus()
   }
 }
 </script>
