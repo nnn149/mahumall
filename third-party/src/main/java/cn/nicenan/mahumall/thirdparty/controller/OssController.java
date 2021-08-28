8da301094f8a8d6fcfffb3fe8be7c243f023e982
Nannan <1041836312@qq.com>
2021-08-28 17:27:22 +0800

    阿里云oss签名生成

---
+++ third-party/src/main/java/cn/nicenan/mahumall/thirdparty/controller/OssController.java
@@ -0,0 +1,62 @@
+package cn.nicenan.mahumall.thirdparty.controller;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class OssController {
+    @Autowired
+    private OSS ossClient;
+
+    @Value("${alibaba.cloud.access-key}")
+    private String accessId;
+    @Value("${alibaba.cloud.oss.endpoint}")
+    private String endpoint;
+    @Value("${alibaba.cloud.oss.bucket}")
+    private String bucket;
+
+    @RequestMapping("/oss/policy")
+    public Map<String, String> policy() {
+        String host = "https://" + bucket + "." + endpoint;
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = date + "/";
+        Map<String, String> respMap = new LinkedHashMap<String, String>();
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            ossClient.shutdown();
+        }
+        return respMap;
+    }
+}
