e194204b7db18fad5aa26def1924e915206a380c
Nannan <1041836312@qq.com>
2021-09-02 14:13:05 +0800

    复制做好的页面

--- product/src/main/java/cn/nicenan/mahumall/product/service/impl/BrandServiceImpl.java
+++ product/src/main/java/cn/nicenan/mahumall/product/service/impl/BrandServiceImpl.java
@@ -1,16 +1,17 @@
 package cn.nicenan.mahumall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import cn.nicenan.mahumall.common.utils.PageUtils;
 import cn.nicenan.mahumall.common.utils.Query;
-
 import cn.nicenan.mahumall.product.dao.BrandDao;
 import cn.nicenan.mahumall.product.entity.BrandEntity;
 import cn.nicenan.mahumall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.mysql.cj.util.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("brandService")
@@ -18,9 +19,14 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        String key = (String) params.get("key");
+        QueryWrapper<BrandEntity> queryWrapper = new QueryWrapper<>();
+        if (!StringUtils.isNullOrEmpty(key)) {
+            queryWrapper.eq("brand_id", key).or().like("name", key);
+        }
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
