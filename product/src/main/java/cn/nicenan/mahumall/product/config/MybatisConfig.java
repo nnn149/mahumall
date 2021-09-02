525e9e7c857f73372572d8c937d8aec499f03b5b
Nannan <1041836312@qq.com>
2021-09-02 14:13:21 +0800

    MybatisPlus分页插件

---
+++ product/src/main/java/cn/nicenan/mahumall/product/config/MybatisConfig.java
@@ -0,0 +1,22 @@
+package cn.nicenan.mahumall.product.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement //开启事务
+@MapperScan("cn.nicenan.mahumall.product.dao")
+public class MybatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+}
