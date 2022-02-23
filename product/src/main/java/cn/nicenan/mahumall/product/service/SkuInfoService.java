24c0e6e9a5d93a681f3b056a3fa70f33880b1346
Nannan <1041836312@qq.com>
2022-02-23 14:40:39 +0800

    210 异步编排

--- product/src/main/java/cn/nicenan/mahumall/product/service/SkuInfoService.java
+++ product/src/main/java/cn/nicenan/mahumall/product/service/SkuInfoService.java
@@ -7,6 +7,7 @@ import cn.nicenan.mahumall.product.entity.SkuInfoEntity;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 /**
  * sku信息
@@ -23,6 +24,6 @@ public interface SkuInfoService extends IService<SkuInfoEntity> {
 
     List<SkuInfoEntity> getSkusBySpuId(Long spuId);
 
-    SkuItemVo item(Long skuId);
+    SkuItemVo item(Long skuId) throws ExecutionException, InterruptedException;
 }
 
