ef6319fbdcdf1764f0ff1dabad6d4cdc00e508be
Nannan <1041836312@qq.com>
2022-02-23 14:23:59 +0800

    210 自定义线程池配置

---
+++ product/src/main/java/cn/nicenan/mahumall/product/config/MyThreadConfig.java
@@ -0,0 +1,23 @@
+package cn.nicenan.mahumall.product.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+//读取的compoment已经注入
+//@EnableConfigurationProperties(ThreadPoolConfigProperties.class)
+@Configuration
+public class MyThreadConfig {
+    @Bean
+    public ThreadPoolExecutor threadPoolExecutor(ThreadPoolConfigProperties properties) {
+        return new ThreadPoolExecutor(properties.getCorePoolSize(), properties.getMaxPoolSize(), properties.getKeepAliveSeconds(),
+                TimeUnit.SECONDS,
+                new java.util.concurrent.LinkedBlockingQueue<>(properties.getQueueCapacity()),
+                Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+    }
+}
