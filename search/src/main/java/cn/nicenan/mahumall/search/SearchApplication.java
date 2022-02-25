f12a83f6269e04c1233a3d7425a658c1e70649d1
Nannan <1041836312@qq.com>
2022-02-25 16:12:44 +0800

    p230 搜索服务共享session

--- search/src/main/java/cn/nicenan/mahumall/search/SearchApplication.java
+++ search/src/main/java/cn/nicenan/mahumall/search/SearchApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
